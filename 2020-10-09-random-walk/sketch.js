fe9faf69b2a91e99c54cb2e86a5993a0cf80e42d
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-10-09 14:42:05 +0100

    Add steering bias to random walk

--- 2020-10-09-random-walk/sketch.js
+++ 2020-10-09-random-walk/sketch.js
@@ -3,13 +3,28 @@ function Walker ( x, y )
     this.x = x;
     this.y = y;
 
-    this.move = function ()
+    this.move = function ( hor, ver )
     {
-        let x_r = -1 + random ( 2 );
-        this.x += x_r;
+        let dir = function ( bias )
+        {
+            let r = random ( 3 ) + bias;
 
-        let y_r = -1 + random ( 2 );
-        this.y += y_r;
+            if ( r < 1 ) 
+            {
+                return -1;
+            }
+            else if ( r < 2 )
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        };
+
+        this.x += dir ( hor );
+        this.y += dir ( ver );
     };
 
     this.draw = function ()
@@ -18,15 +33,23 @@ function Walker ( x, y )
     };
 }
 
-var w = [];
+let w = [];
+
+let left_slider, right_slider;
 
 function setup() 
 {
     createCanvas(400, 400);
 
+    left_slider = createSlider ( -1, 1, 0, 0.1 );
+    left_slider.position ( 20, 20 );
+
+    right_slider = createSlider ( -1, 1, 0, 0.1 );
+    right_slider.position ( 20, 50 );
+
     stroke ( 'rgba(0%, 0%, 0%, 0.1)' );
 
-    for ( let i = 0; i < 100; i++ )
+    for ( let i = 0; i < 10; i++ )
     {
         w.push ( new Walker ( 200, 200 ) );
     }
@@ -34,9 +57,12 @@ function setup()
 
 function draw() 
 {
+    text ( 'horizontal', left_slider.x *2 + left_slider.width, 35 );
+    text ( 'vertical', right_slider.x *2 + right_slider.width, 65 );
+
     w.forEach ( ( walker ) => 
     {
-        walker.move ();
+        walker.move ( left_slider.value (), right_slider.value () );
         walker.draw ();
     });
 }
