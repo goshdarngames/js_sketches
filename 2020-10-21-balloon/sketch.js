01f3d561368867f3e090e7fc27f6ff918b0c735a
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-10-21 15:35:54 +0100

    Balloon sketch WIP 2

--- 2020-10-21-balloon/sketch.js
+++ 2020-10-21-balloon/sketch.js
@@ -1,3 +1,10 @@
+let minLoad = 1;
+let maxLoad = 20;
+
+let maxInflation = 3;
+
+let gravity = 1;
+
 class Balloon
 {
     constructor ( pos, load, inflation )
@@ -7,21 +14,59 @@ class Balloon
         this.pos = pos;
 
         this.inflation = inflation
+
+        this.velocity = createVector ( 0, 0 );
     }
 
-    update ()
+    update ( wind )
     {
+        let acceleration = createVector ( 0, 0 );
+
+        let lift = ( this.load - this.inflation ) / maxLoad;
+
+        acceleration.add ( createVector ( 0, lift ) );
+
+        acceleration.add ( createVector ( 0, gravity ) );
+
+        if ( this.pos.y < height - 10 )
+            acceleration.add ( createVector ( wind, 0 ) );
+
+        this.velocity.add ( acceleration );
+
+        this.pos.add ( this.velocity );
+
+        if ( this.pos.y > height )
+        {
+            this.pos.y = height;
+            this.velocity = createVector ( 0, 0 );
+        }
+
+        if ( this.pos.y < height / 3 )
+            this.inflation = 0;
+
+        this.pos.x = ( width + this.pos.x ) % width;
+
+        this.inflation = Math.min ( this.inflation + 0.1, 
+            maxLoad * maxInflation );
     }
 
     display ()
     {
+        rectMode ( RADIUS );
         square ( this.pos.x, this.pos.y, this.load );
 
-        circle ( this.pos.x, this.pos.y - this.inflation/2, this.inflation/2 );
+        let b_rad = this.inflation;
+
+        let b_x = this.pos.x;
+        let b_y = this.pos.y - b_rad - this.load;
+
+        circle ( b_x, b_y, b_rad * 2 );
     }
 }
 
 let balloons = [];
+
+let wind;
 
 function setup() 
 {
@@ -29,12 +74,12 @@ function setup()
 
     for ( let i=0; i < 10; i++ )
     {
-        let load = random ( 10, 80 );
+        let load = random ( minLoad, maxLoad );
 
         let pos = createVector ( random ( load, width-load ),
                                  random ( load, height-load ) );
 
-        let inflation = random ( 0, 300 );
+        let inflation = random ( 0, maxInflation );
 
         balloons.push ( new Balloon ( pos, load, inflation ) );
     }
@@ -44,9 +89,11 @@ function draw()
 {
     background ( '#884477' );
 
+    wind = random ( -1, 1 );
+
     balloons.forEach ( ( b ) =>
     {
-        b.update ();
+        b.update ( wind );
         b.display ();
     });
 }
