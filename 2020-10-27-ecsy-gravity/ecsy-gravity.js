f35f01cce7b210dfe260710d1b0cd96845000ea8
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-10-27 14:24:44 +0000

    Ecsy gravity

--- 2020-10-27-ecsy-gravity/ecsy-gravity.js
+++ 2020-10-27-ecsy-gravity/ecsy-gravity.js
@@ -2,8 +2,7 @@ class Position extends ECSY.Component {}
 
 Position.schema =
 {
-    x : { type : ECSY.Types.Number},
-    y : { type : ECSY.Types.Number},
+    p : { type : ECSY.Types.Ref },
 }
 
 class Velocity extends ECSY.Component {}
@@ -13,7 +12,106 @@ Velocity.schema =
     v : { type : ECSY.Types.Ref },
 }
 
+class Acceleration extends ECSY.Component {}
+
+Acceleration.schema =
+{
+    a : { type : ECSY.Types.Ref },
+}
+
+class Mass extends ECSY.Component {}
+
+Mass.schema =
+{
+    m : { type : ECSY.Types.Number },
+}
+
 class DisplayCircle extends ECSY.TagComponent {};
+
+class GravitySystem extends ECSY.System
+{
+    init () {}
+
+    gravData ( b )
+    {
+        let data = 
+        {
+            pos  : b.getComponent ( Position ).p,
+            mass : b.getComponent ( Mass ).m,
+            acc  : b.getComponent ( Acceleration ).a,
+        };
+
+        return data;
+    }
+
+    attract ( b1, b2 )
+    {
+        let d1 = this.gravData ( b1 );
+        let d2 = this.gravData ( b2 );
+
+        let f = p5.Vector.sub ( d2.pos, d1.pos );
+
+        let dist = constrain ( f.mag (), 5, 40 );
+
+        f.normalize ();
+
+        let g = 1;
+
+        let strength = ( g * d1.mass * d2.mass ) / ( dist * dist );
+
+        f.mult ( strength );
+
+        d1.acc.add ( f );
+    }
+
+    execute ( delta, time )
+    {
+        this.queries.bodies.results.forEach( b1 => 
+        {
+            this.queries.bodies.results.forEach( b2 => 
+            {
+                if ( b1 != b2 ) this.attract ( b1, b2 );
+            });
+        });
+    }
+}
+
+GravitySystem.queries = 
+{
+    bodies : 
+    {
+        components : [ Position, Acceleration, Mass ]
+    }
+};
+
+class MovementSystem extends ECSY.System
+{
+    init () {}
+
+    execute ( delta, time )
+    {
+        this.queries.movers.results.forEach(entity => {
+
+            let pos = entity.getComponent ( Position ).p;
+            let v = entity.getComponent ( Velocity ).v;
+            let acc = entity.getComponent ( Acceleration ).a;
+
+            v.add ( acc );
+
+            acc.set ( 0 );
+
+            pos.add ( v );
+        });
+    }
+}
+
+MovementSystem.queries = 
+{
+    movers : 
+    {
+        components : [ Position, Velocity, Acceleration ]
+    }
+};
 
 class DisplaySystem extends ECSY.System
 {
@@ -23,7 +121,7 @@ class DisplaySystem extends ECSY.System
     {
         this.queries.circles.results.forEach(entity => {
 
-            let pos = entity.getComponent ( Position );
+            let pos = entity.getComponent ( Position ).p;
 
             circle ( pos.x, pos.y, 10 );
         });
--- 2020-10-27-ecsy-gravity/sketch.js
+++ 2020-10-27-ecsy-gravity/sketch.js
@@ -7,33 +7,49 @@ function setup()
     world
         .registerComponent ( Position )
         .registerComponent ( DisplayCircle )
+        .registerComponent ( Acceleration )
+        .registerComponent ( Mass )
         .registerComponent ( Velocity );
 
     world
+        .registerSystem ( MovementSystem )
+        .registerSystem ( GravitySystem )
         .registerSystem ( DisplaySystem );
 
     for ( let i = 0; i < 10; i++ )
     {
         let pos = 
         {
-            x : random ( 0, width ),
-            y : random ( 0, height ),
-        };
+            p : createVector ( random ( 0, width ), random ( 0, height ) ),
+        }
 
         let velocity = 
         {
             v : p5.Vector.random2D (),
         }
 
+        let acc = 
+        {
+            a : createVector ( 0, 0 ),
+        }
+
+        let mass = 
+        {
+            m : 20,
+        }
+
         world
             .createEntity ()
             .addComponent ( Position, pos )
             .addComponent ( Velocity, velocity )
+            .addComponent ( Acceleration, acc )
+            .addComponent ( Mass, mass )
             .addComponent ( DisplayCircle );
     }
 }
 
 function draw() 
 {
+    background ( 123 );
     world.execute ( deltaTime, millis () );
 }
