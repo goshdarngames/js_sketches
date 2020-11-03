0b6ca9ab3d8cdf3f6ee71c03ac33a1332de54001
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-11-03 14:54:56 +0000

    Harmonic motion controls

--- 2020-11-03-harmonic-motion/sketch.js
+++ 2020-11-03-harmonic-motion/sketch.js
@@ -2,35 +2,71 @@ let amp;
 
 let period = 60 * 6;
 
-let n = 90;
+let n_slider;
 
-let r = 20;
+let rad = 20;
 
-let colors = [ 'red', 'green', 'blue' ];
+let colors = [ '#ff0', '#0ff', '#f0f' ];
+
+let f = 6;
+let f1_slider;
+
+let ui_y;
+let ui = []
+
+let f2_slider;
+let bg_slider;
 
 function setup() 
 {
     createCanvas(800, 800);
 
     amp = width/2;
+
+    ui_y = height + 26;
+
+    new_slider (1, 300, 12, 1);
+    new_slider (1,60,10,1);
+    new_slider (1,60,1,1);
+    new_slider (1,255,255,1);
+    n_slider  = ui [ 0 ];
+    f1_slider = ui [ 1 ];
+    f2_slider = ui [ 2 ];
+    bg_slider = ui [ 3 ];
+    
+}
+
+function new_slider ( min, max, def, step )
+{
+    let s = createSlider ( min, max, def, step );
+    s.position ( 80, ui_y );
+    s.size ( 600, AUTO );
+
+    ui.push ( s );
+
+    ui_y += 40;
 }
 
 function draw() 
 {
-    background ( 77 );
+    background ( 40, 20, 12, bg_slider.value () );
 
     translate ( width/2, height/2 );
 
+    let n = n_slider.value ();
+
+    let f2 = f2_slider.value ();
+
     for ( let i = 0; i < n; i++ )
     {
-        let off = TWO_PI * i/n;
+        let off = f2 * TWO_PI * i/n;
 
         let x = amp * cos ( off + TWO_PI * frameCount / period );
 
-        rotate ( TWO_PI / n );
+        rotate ( f1_slider.value () * TWO_PI / n );
 
         fill ( colors [ i % colors.length ] );
 
-        circle ( x, 0, r );
+        circle ( x, 0, rad );
     }
 }
