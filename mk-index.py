ec5604f29a11f03c83410cee410a031dad9544a0
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-11-09 15:49:52 +0000

    Generate index html for sketches

--- mk-index.py
+++ mk-index.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python3
 
+import os
+import re
+
 print ( "Generating index..." )
+
+def mk_sketch_list_item ( sketch_name ):
+
+    item = "<li>"
+    item += "<a href='"
+    item += os.path.join ( "sketches", sketch_name, "index.html" )
+    item += "'>"+sketch_name+"</a>"
+    item += "</li>"
+
+    return item
 
 def mk_sketch_list ():
 
-    out = "<li>"
+    out = "<ul>"
 
-    out += "</li>"
+    for d in sorted ( os.listdir ( 'sketches' ), reverse=True ):
+
+        out += mk_sketch_list_item ( d )
+
+    out += "</ul>"
 
     return out
 
-print ( mk_sketch_list () );
+out_buffer = []
 
-"""
 with open ( "index/index.html" ) as source_file:
 
     for cnt, line in enumerate ( source_file ):
 
-        print ( "Line {}: {}".format ( cnt, line ) )
-"""
+        if ( re.match ( ".*<!--SKETCHES-->", line ) ):
+
+            out_buffer.append ( mk_sketch_list () )
+
+        else:
+
+            out_buffer.append ( line )
+
+
+with open ( "index.html", 'w' ) as out_file:
+
+    for line in out_buffer:
+
+        out_file.write ( line )
