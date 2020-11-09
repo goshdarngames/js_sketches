092560fb25c660a1e1c4cf8968e193b781a40a95
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2020-11-09 11:46:26 +0000

    Add files for generating sketch index.

---
+++ mk-index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+print ( "Generating index..." )
+
+def mk_sketch_list ():
+
+    out = "<li>"
+
+    out += "</li>"
+
+    return out
+
+print ( mk_sketch_list () );
+
+"""
+with open ( "index/index.html" ) as source_file:
+
+    for cnt, line in enumerate ( source_file ):
+
+        print ( "Line {}: {}".format ( cnt, line ) )
+"""
